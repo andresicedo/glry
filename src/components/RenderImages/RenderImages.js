24f06db7b62c4e0953e9654b173f5711bcb06f8f
Andres Icedo <andresicedo24@gmail.com>
2021-07-09 13:38:54 -0500

    updated gallery grid

--- src/components/RenderImages/RenderImages.js
+++ src/components/RenderImages/RenderImages.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from 'react';
+import './RenderImages.css';
+import SampleImg from '../../assets/images/sample.jpg';
+
 
 export default function RenderImages() {
+
+
     return (
-        <div>
-            Render Images
+        <div style={{ marginTop: '15px' }}>
+            <div className="gallery">
+                <div className='gallery__item gallery__item--1'>
+                    <img src={SampleImg} alt="galleryImg" className="galleryImg" />
+                </div>
+                <div className='gallery__item gallery__item--2'>
+                    <img src={SampleImg} alt="galleryImg" className="galleryImg" />
+                </div>
+                <div className='gallery__item gallery__item--3'>
+                    <img src={SampleImg} alt="galleryImg" className="galleryImg" />
+                </div>
+                <div className='gallery__item gallery__item--4'>
+                    <img src={SampleImg} alt="galleryImg" className="galleryImg" />
+                </div>
+                <div className='gallery__item gallery__item--5'>
+                    <img src={SampleImg} alt="galleryImg" className="galleryImg" />
+                </div>
+                <div className='gallery__item gallery__item--6'>
+                    <img src={SampleImg} alt="galleryImg" className="galleryImg" />
+                </div>
+            </div>
         </div>
     )
 }
